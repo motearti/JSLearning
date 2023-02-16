7ff22faef27ebcd4c5a453324a3125f922518fb3
motearti <motearti@gmail.com>
2023-02-16 12:04:51 +0530

    update variable declaration and initialization

---
+++ 01_variable_assign.js
@@ -0,0 +1,24 @@
+console.log("***********************step 1.*********************");
+var myFullName;
+console.log(myFullName);
+myFullName= "Arti Mote";
+console.log(myFullName);
+myFullName="Arti Rama Mote";
+console.log(myFullName);
+
+
+console.log("*********************step 2.*************************");
+var myLove;
+myLove="my love= I Love Only JS";
+console.log(myLove);
+
+
+console.log("************************step 3.************************");
+var firstName="firstName= Arti", lastName="lastName= mote", age="age= 24" , address="address= at-sangola dist-solapur", pincode="pincode= 413307", gender="gender= female", isMarried="isMarried= Yes";
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(address);
+console.log(pincode);
+console.log(gender);
+console.log(isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,22 @@
 console.log("MOHIT SHARMA");
+console.log("24");
+console.log("solapur");
+
+var fullName="arti mote";
+console.log(fullName);
+console.log(fullName);
+
+ var city; // variable declaration
+console.log(city);
+ city="pune"; //variable initialization
+console.log(city);
+ city="surat"; //variable update
+console.log(city);
+
+var age="24";//variable declaration and initialization in same line
+age="25"
+age="26";
+console.log(age);
+
+var university_name ="solapur"
+console.log(university_name);
