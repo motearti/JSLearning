37c2624473e1326a107707e79808981ff838e101
motearti <motearti@gmail.com>
2023-02-17 17:36:30 +0530

    update file with type of variable

---
+++ 03_variableBassics.js
@@ -0,0 +1,18 @@
+
+console.log("-----------------------------(number)--------------------------");
+var age=24;
+console.log("value of age variable is:",age);
+console.log("typeOf age is:", typeof age);
+
+
+console.log("------------------------(string)--------------------------------");
+var city="banglore";
+console.log("value of city variable is:",city);
+console.log("typeOf  city is:",typeof city);
+
+
+console.log("------------------------(boolean)--------------------------------");
+var isMarried=true ;
+console.log("value of isMarried :",isMarried);
+console.log("typeOf isMarried : ", typeof isMarried);
+
---
+++ 04_typeof.js
@@ -0,0 +1,27 @@
+console.log("-----------------------------step1----------------------");
+var bankName ;
+console.log(bankName);
+console.log("typeOf bankName is:",typeof bankName);
+bankName = "SBI Bank"
+console.log(bankName);
+console.log("typeOf bankName:", typeof bankName);
+
+console.log("---------------------------step2---------------");
+var mark = "90%";
+console.log("value of variable mark is:",mark);
+console.log("typeOf mark:", typeof mark);
+
+console.log("--------------------------step3-------------------");
+var isWorking;
+console.log(isWorking);
+isWorking= true;
+console.log("value of variable isWorking is:",isWorking);
+console.log("typeOf isWorking is:",typeof isWorking)
+
+console.log("---------------------------step4----------------------");
+var totalCount = "Hundred and Seven";
+console.log("value of variable totalCount is:",totalCount);
+console.log("typeOf totalCount is:",typeof totalCount);
+totalCount=107;
+console.log("value of variable totalCount is:",totalCount);
+console.log("typeOf totalCount is:",typeof totalCount);
