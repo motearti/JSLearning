660f6c006ea9850f4e06d56b3a0edd21499cbdfe
motearti <motearti@gmail.com>
2023-02-17 17:35:03 +0530

    update with datatype

---
+++ 02_datatype.js
@@ -0,0 +1,33 @@
+
+var myName = 'Virat Kohli';
+var fatherName = "Yogesh Kohli"
+
+var age = 31;
+var weight = 82.5;
+
+var isMarried = true;
+var isLazy = false; 
+
+var typeOfMyName  = typeof myName;
+console.log("Typeof variable myName: ",  typeOfMyName);
+console.log("typeOf variable fatherName:", typeof fatherName);
+
+console.log("typeOf age is:", typeof age);
+console.log("typeOf weight is:",typeof weight);
+
+console.log("Typeof variable isMarried: ",  typeof isMarried);
+console.log("typeOf variable isLazy:", typeof isLazy);
+
+
+var graduation;
+console.log("Value of  graduation variable is: ", graduation);
+console.log("typeOf graduation is: ",typeof graduation);
+
+
+var city=null;
+console.log("value of city variable is: ",city);
+console.log( "typeof city is:",typeof city);
+
+age=100;
+console.log(typeof age);
+
