96f20a346e33ce64ca19f9a034e66161dc774236
motearti <motearti@gmail.com>
2023-02-22 21:42:38 +0530

    update with string templates

---
+++ 04_stringTemplete.js
@@ -0,0 +1,4 @@
+
+console.log(` you are "champ" man`);
+console.log("My First name ", firstName, "Last name ", lastName, " City ", city);
+console.log(`My First name${ firstName}Last name ${ lastName} City${ city}`);
