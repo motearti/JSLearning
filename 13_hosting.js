d02471de02dc756b9ad6b1bd5afb83ece1e6d67f
motearti <motearti@gmail.com>
2023-03-16 21:27:30 +0530

    update with hosting

---
+++ 13_hosting.js
@@ -0,0 +1,17 @@
+
+console.log(myName);
+
+var myName =("fullName");
+age= 30;
+
+show();
+
+function show() {
+    console.log("show() function");
+}
+
+//greet(); not allowed as it is funExp and fe does not hosted
+var greet = function (){
+    console.log("Good Morning");
+}
+greet();
