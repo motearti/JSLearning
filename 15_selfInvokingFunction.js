01bf49a934ba8d6fe86f10689c92bbfbab6aa269
motearti <motearti@gmail.com>
2023-03-21 00:18:07 +0530

    update with ECMA6 features

---
+++ 15_selfInvokingFunction.js
@@ -0,0 +1,6 @@
+
+//Self Invoking Function or Immediately invoked function Expression
+
+ (function(){
+    console.log("Inside Self Invoking Function or Immediately invoked function Expression");
+})();
---
+++ 15_strictMode.js
@@ -0,0 +1,22 @@
+
+'use strict'
+
+//myName = "Virat"; // Not allowed
+let myName = "Virat";
+console.log(myName);
+
+
+person = {  // Not allowed to define an object without var, let and const
+    firstName: "Virat",
+    lastName: "Kohli",
+    age : 33
+}
+
+
+const student = {
+    rollNo: 34,
+    age: 17,
+    city: "PUNE"
+}
+
+// delete student; // Not allowed in strict mode
